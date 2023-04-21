d636d20fe01dc389aa2d7ed278aa2797ac201e8a
shawnchampion <shawnchampion2016@gmail.com>
2023-04-21 15:29:00 +0800

    edit web title

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,10 +7,10 @@ import { getBuildConfig } from "./config/build";
 const buildConfig = getBuildConfig();
 
 export const metadata = {
-  title: "ChatGPT Next Web",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "Talk to think",
+  description: "Your personal Chat Bot.",
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "Talk to think",
     statusBarStyle: "default",
   },
   themeColor: "#fafafa",
